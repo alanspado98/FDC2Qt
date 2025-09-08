a99e55efd3e64b50b1bd093854db42e40021ddff
alanspado98 <151512285+alanspado98@users.noreply.github.com>
2025-09-08 10:12:34 +0200

    Update DATASET.R

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,7 +5,7 @@ usethis::use_data(DATASET, overwrite = TRUE)
 
 #"data" folder with created datasets
 
-usethis::use_data(hydrometric_levels)
+usethis::use_data(stream_stages)
 #compress="bzip2",“gzip”,“xz” 
 usethis::use_data(rating_curves)
 usethis::use_data(streamflows)
